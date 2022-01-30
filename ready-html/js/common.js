40afb65a4bf44c6582026bf14f4daf4788bbe3e1
RustamKeneev <dr.rustamkeneev@gmail.com>
2022-01-30 23:57:47 +0600

    start added carousel

--- ready-html/js/common.js
+++ ready-html/js/common.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded',function (){
 
+    const showSlider = new Swiper('.showcase-carousel',{
+        loop: true,
+        slidesPerView: 3,
+        speed: 1800
+    })
 
     document.querySelector('video').playbackRate = 2
 })
